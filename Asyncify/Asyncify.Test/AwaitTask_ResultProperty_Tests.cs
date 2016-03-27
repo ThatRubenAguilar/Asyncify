19e0d1c2ee51a9b94e6d464fd159f776496c8a63
Ruben Aguilar <ThatRubenAguilar@gmail.com>
2016-03-27 01:34:46 -0700

    Add parenthesis tests for Await Task Fix on Result Property

--- Asyncify/Asyncify.Test/AwaitTask_ResultProperty_Tests.cs
+++ Asyncify/Asyncify.Test/AwaitTask_ResultProperty_Tests.cs
@@ -35,5 +35,125 @@ namespace Asyncify.Test
             AwaitTaskDiagnosticAndFix(testExpression, expected, fixExpression);
         }
 
+        [TestMethod]
+        public void Should_add_parenthesis_to_await_task_fix_on_generic_task_result_property_when_using_task_field()
+        {
+            var testExpression = @"var val = AsyncMethods.GetMemberMethods().Result.Field1;";
+            var expected = new DiagnosticResult
+            {
+                Id = ConsiderAwaitOverResultAnalyzer.DiagnosticId,
+                Message = String.Format(ConsiderAwaitOverResultAnalyzer.MessageFormat.ToString(),
+                                            "AsyncMethods.GetMemberMethods()"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 9, 51)
+                        }
+            };
+            var fixExpression = @"var val = (await AsyncMethods.GetMemberMethods()).Field1;";
+
+            AwaitTaskDiagnosticAndFix(testExpression, expected, fixExpression);
+        }
+
+        [TestMethod]
+        public void Should_not_add_parenthesis_to_await_task_fix_on_already_parenthesized_generic_task_result_property_when_using_task_field()
+        {
+            var testExpression = @"var val = (AsyncMethods.GetMemberMethods().Result).Field1;";
+            var expected = new DiagnosticResult
+            {
+                Id = ConsiderAwaitOverResultAnalyzer.DiagnosticId,
+                Message = String.Format(ConsiderAwaitOverResultAnalyzer.MessageFormat.ToString(),
+                                            "AsyncMethods.GetMemberMethods()"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 9, 52)
+                        }
+            };
+            var fixExpression = @"var val = (await AsyncMethods.GetMemberMethods()).Field1;";
+
+            AwaitTaskDiagnosticAndFix(testExpression, expected, fixExpression);
+        }
+
+        [TestMethod]
+        public void Should_add_parenthesis_to_await_task_fix_on_generic_task_result_property_when_using_task_property()
+        {
+            var testExpression = @"var val = AsyncMethods.GetMemberMethods().Result.Property1;";
+            var expected = new DiagnosticResult
+            {
+                Id = ConsiderAwaitOverResultAnalyzer.DiagnosticId,
+                Message = String.Format(ConsiderAwaitOverResultAnalyzer.MessageFormat.ToString(),
+                                            "AsyncMethods.GetMemberMethods()"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 9, 51)
+                        }
+            };
+            var fixExpression = @"var val = (await AsyncMethods.GetMemberMethods()).Property1;";
+
+            AwaitTaskDiagnosticAndFix(testExpression, expected, fixExpression);
+        }
+
+        [TestMethod]
+        public void Should_not_add_parenthesis_to_await_task_fix_on_already_parenthesized_generic_task_result_property_when_using_task_property()
+        {
+            var testExpression = @"var val = (AsyncMethods.GetMemberMethods().Result).Property1;";
+            var expected = new DiagnosticResult
+            {
+                Id = ConsiderAwaitOverResultAnalyzer.DiagnosticId,
+                Message = String.Format(ConsiderAwaitOverResultAnalyzer.MessageFormat.ToString(),
+                                            "AsyncMethods.GetMemberMethods()"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 9, 52)
+                        }
+            };
+            var fixExpression = @"var val = (await AsyncMethods.GetMemberMethods()).Property1;";
+
+            AwaitTaskDiagnosticAndFix(testExpression, expected, fixExpression);
+        }
+
+        [TestMethod]
+        public void Should_add_parenthesis_to_await_task_fix_on_generic_task_result_property_when_using_task_method()
+        {
+            var testExpression = @"var val = AsyncMethods.GetMemberMethods().Result.GetNumber();";
+            var expected = new DiagnosticResult
+            {
+                Id = ConsiderAwaitOverResultAnalyzer.DiagnosticId,
+                Message = String.Format(ConsiderAwaitOverResultAnalyzer.MessageFormat.ToString(),
+                                            "AsyncMethods.GetMemberMethods()"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 9, 51)
+                        }
+            };
+            var fixExpression = @"var val = (await AsyncMethods.GetMemberMethods()).GetNumber();";
+
+            AwaitTaskDiagnosticAndFix(testExpression, expected, fixExpression);
+        }
+
+        [TestMethod]
+        public void Should_not_add_parenthesis_to_await_task_fix_on_already_parenthesized_generic_task_result_property_when_using_task_method()
+        {
+            var testExpression = @"var val = (AsyncMethods.GetMemberMethods().Result).GetNumber();";
+            var expected = new DiagnosticResult
+            {
+                Id = ConsiderAwaitOverResultAnalyzer.DiagnosticId,
+                Message = String.Format(ConsiderAwaitOverResultAnalyzer.MessageFormat.ToString(),
+                                            "AsyncMethods.GetMemberMethods()"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 9, 52)
+                        }
+            };
+            var fixExpression = @"var val = (await AsyncMethods.GetMemberMethods()).GetNumber();";
+
+            AwaitTaskDiagnosticAndFix(testExpression, expected, fixExpression);
+        }
+
     }
 }
